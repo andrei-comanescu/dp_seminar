840bc088158723b8c399d454eeecbd3b3e63bc56
andrei-comanescu <andreidaniel.comanescu@gmail.com>
2019-04-05 13:20:50 +0300

    added the laplace mechanism

--- dp_algorithms.py
+++ dp_algorithms.py
@@ -26,7 +26,40 @@ def gaussian_mechanism(data_matrix, epsilon, delta):
 
         return empirical_covariance_matrix
 
+
+def laplace_mechanism(data_matrix, epsilon):
+
+    variance = 2 * data_matrix.shape[1] / (data_matrix.shape[0] * epsilon)
+
+    if variance <= 0:
+
+        print("This is not going to work out well for you so I am just going to stop now ...")
+        print("Variance needs to be bigger than 0")
+        print("Bye")
+
+        return -1
+
+    else:
+
+        # calculate the empirical covariance matrix
+        empirical_covariance_matrix = np.matmul(data_matrix.transpose(), data_matrix)
+        empirical_covariance_matrix /= len(data_matrix)
+
+        # draw the noise and construct the symmetrical noise matrix
+        noise = np.random.laplace(0, variance, (len(empirical_covariance_matrix), len(empirical_covariance_matrix)))
+        noise_matrix = np.tril(noise) + np.tril(noise, -1).T
+
+        empirical_covariance_matrix += noise_matrix
+
+        return empirical_covariance_matrix
+
+
 def wishart_mechanism(data_matrix, epsilon):
+    '''
+        The code works BUT, the mechanism in itself does NOT offer differencial privacy;
+        the paper that proposed said mechanism was retracted, more information here:
+        https://ergodicity.net/2017/04/07/retraction-for-symmetric-matrix-perturbation-for-differentially-private-principal-component-analysis-icassp-2016/
+    '''
 
     # the data matrix is a pandas frame
 
